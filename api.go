e8f43e2742e234866182509069172bbceb4d0864
sun-moon-star-star <75414051+sun-moon-star-star@users.noreply.github.com>
2021-03-09 10:43:38 +0800

    feat(api.go): delete RetryConfig

--- api.go
+++ api.go
@@ -2,19 +2,7 @@ package errors
 
 import "error"
 
-type RetryConfig struct{
-	Req []byte
-	Resp []byte
-
-	RetryTimes int16 // -1 means until success
-	RetryInterval int32  // millisecond
-	
-	IP string
-	Port uint16
-	Protocol string
-}
-
 type API struct {
-	Record ([]byte) error // Record error
-	Retry(*RetryConfig) error // 
+	RecordError ([]byte) error // Record error
+	Retry(*caller.Caller) error // 
 }
